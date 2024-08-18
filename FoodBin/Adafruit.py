8b44b2d1754aa6de7151114744e7379f90a8c262
Robert Heller <heller@deepsoft.com>
2024-08-17 21:28:45 -0400

    Add additional I2C boards.

--- FoodBin/Adafruit.py
+++ FoodBin/Adafruit.py
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Tue Aug 13 18:16:55 2024
-#  Last Modified : <240814.1603>
+#  Last Modified : <240817.2113>
 #
 #  Description	
 #
@@ -141,14 +141,153 @@ class AdafruitTFTFeatherWing(object):
         obj.Label=self.name+'_shortHeader'
         obj.ViewObject.ShapeColor=tuple([0.2,0.2,0.2])
         
+class AdafruitNAU7802(object):
+    __BoardLength = 1.0*25.4
+    __BoardWidth  = 0.9*25.4
+    __BoardMountingHolesYZInches = [(.1,.1),(.9,.1),(.9,.8),(.1,.8)]
+    __BoardMountingHolesRadius = (.1*25.4)/2
+    __BoardThick = 1.6
+    def __init__(self,name,origin):
+        self.name = name
+        if not isinstance(origin,Base.Vector):
+            raise RuntimeError("origin is not a Vector")
+        self.origin = origin
+        self.board = Part.makePlane(self.__BoardWidth,self.__BoardLength,\
+                                    origin,Base.Vector(1,0,0))\
+                           .extrude(Base.Vector(self.__BoardThick,0,0))
+        self.MountingHoles = list()
+        for Yin,Zin in self.__BoardMountingHolesYZInches:
+            self.MountingHoles.append(origin.add(Base.Vector(0,-Yin*25.4,Zin*25.4)))
+        #self.holes = list()
+        for i in range(0,4):
+            h = self.MakeMountingHole(i,origin.x,self.__BoardThick)
+            #self.holes.append(h)
+            self.board = self.board.cut(h)
+    def MakeMountingHole(self,index,X,Xdelta):
+        holeOrig = Base.Vector(X,self.MountingHoles[index].y,self.MountingHoles[index].z)
+        hole = Part.Face(Part.Wire(Part.makeCircle(self.__BoardMountingHolesRadius,\
+                                                   holeOrig,\
+                                                   Base.Vector(1,0,0))))\
+                    .extrude(Base.Vector(Xdelta,0,0))
+        return hole
+    def show(self,doc=None):
+        if doc==None:
+            doc = App.activeDocument()
+        obj = doc.addObject("Part::Feature",self.name+'_board')
+        obj.Shape = self.board
+        obj.Label=self.name+'_board'
+        obj.ViewObject.ShapeColor=tuple([0.0,0.0,0.0])
+        #for h in self.holes:
+        #    obj = doc.addObject("Part::Feature",self.name+'_hole')
+        #    obj.Shape = h
+        #    obj.ViewObject.ShapeColor=tuple([1.0,0.0,0.0])
+
+
+class AdafruitPCF8523(object):
+    __BoardLength = 1.0*25.4
+    __BoardWidth  = 0.7*25.4
+    __BoardMountingHolesYZInches = [(.1,.1),(.9,.1),(.9,.6),(.1,.6)]
+    __BoardMountingHolesRadius = (.1*25.4)/2
+    __BoardThick = 1.6
+    __BatteryHolderThick = .15*25.4
+    __BatteryHolderRadius = .26*25.4
+    @classmethod
+    def RaisedBoardHeight(cls):
+        return cls.__BatteryHolderThick
+    def __init__(self,name,origin):
+        self.name = name
+        if not isinstance(origin,Base.Vector):
+            raise RuntimeError("origin is not a Vector")
+        self.origin = origin
+        self.board = Part.makePlane(self.__BoardWidth,self.__BoardLength,\
+                                    origin,Base.Vector(1,0,0))\
+                           .extrude(Base.Vector(self.__BoardThick,0,0))
+        self.MountingHoles = list()
+        for Yin,Zin in self.__BoardMountingHolesYZInches:
+            self.MountingHoles.append(origin.add(Base.Vector(0,-Yin*25.4,Zin*25.4)))
+        #self.holes = list()
+        for i in range(0,4):
+            h = self.MakeMountingHole(i,origin.x,self.__BoardThick)
+            #self.holes.append(h)
+            self.board = self.board.cut(h)
+        bY = self.__BoardLength/2.0
+        bZ = self.__BoardWidth/2.0
+        bhOrigin = origin.add(Base.Vector(0,-bY,bZ))
+        self.bh = Part.Face(Part.Wire(Part.makeCircle(self.__BatteryHolderRadius,\
+                                                      bhOrigin,\
+                                                      Base.Vector(1,0,0))))\
+                                    .extrude(Base.Vector(-self.__BatteryHolderThick))
+    def MakeMountingHole(self,index,X,Xdelta):
+        holeOrig = Base.Vector(X,self.MountingHoles[index].y,self.MountingHoles[index].z)
+        hole = Part.Face(Part.Wire(Part.makeCircle(self.__BoardMountingHolesRadius,\
+                                                   holeOrig,\
+                                                   Base.Vector(1,0,0))))\
+                    .extrude(Base.Vector(Xdelta,0,0))
+        return hole
+    def show(self,doc=None):
+        if doc==None:
+            doc = App.activeDocument()
+        obj = doc.addObject("Part::Feature",self.name+'_board')
+        obj.Shape = self.board
+        obj.Label=self.name+'_board'
+        obj.ViewObject.ShapeColor=tuple([0.0,0.0,0.0])
+        #for h in self.holes:
+        #    obj = doc.addObject("Part::Feature",self.name+'_hole')
+        #    obj.Shape = h
+        #    obj.ViewObject.ShapeColor=tuple([1.0,0.0,0.0])
+        obj = doc.addObject("Part::Feature",self.name+'_batteryHolder')
+        obj.Shape = self.bh
+        obj.Label=self.name+'_batteryHolder'
+        obj.ViewObject.ShapeColor=tuple([0.3,0.3,0.3])
+
+class Adafruitvl6180x(object):
+    __BoardLength = 1.0*25.4
+    __BoardWidth  = 0.7*25.4
+    __BoardMountingHolesXZInches = [(.1,.1),(.9,.1),(.9,.6),(.1,.6)]
+    __BoardMountingHolesRadius = (.1*25.4)/2
+    __BoardThick = 1.6
+    def __init__(self,name,origin):
+        self.name = name
+        if not isinstance(origin,Base.Vector):
+            raise RuntimeError("origin is not a Vector")
+        self.origin = origin
+        self.board = Part.makePlane(self.__BoardWidth,self.__BoardLength,\
+                                    origin,Base.Vector(0,1,0))\
+                           .extrude(Base.Vector(0,self.__BoardThick,0,))
+        self.MountingHoles = list()
+        for Xin,Zin in self.__BoardMountingHolesXZInches:
+            self.MountingHoles.append(origin.add(Base.Vector(Xin*25.4,0,Zin*25.4)))
+        #self.holes = list()
+        for i in range(0,4):
+            h = self.MakeMountingHole(i,origin.x,self.__BoardThick)
+            #self.holes.append(h)
+            self.board = self.board.cut(h)
+    def MakeMountingHole(self,index,Y,Ydelta):
+        holeOrig = Base.Vector(self.MountingHoles[index].x,Y,self.MountingHoles[index].z)
+        hole = Part.Face(Part.Wire(Part.makeCircle(self.__BoardMountingHolesRadius,\
+                                                   holeOrig,\
+                                                   Base.Vector(0,1,0))))\
+                    .extrude(Base.Vector(0,Ydelta,0))
+        return hole
+    def show(self,doc=None):
+        if doc==None:
+            doc = App.activeDocument()
+        obj = doc.addObject("Part::Feature",self.name+'_board')
+        obj.Shape = self.board
+        obj.Label=self.name+'_board'
+        obj.ViewObject.ShapeColor=tuple([0.0,0.0,0.0])
+        #for h in self.holes:
+        #    obj = doc.addObject("Part::Feature",self.name+'_hole')
+        #    obj.Shape = h
+        #    obj.ViewObject.ShapeColor=tuple([1.0,0.0,0.0])
 
 
 if __name__ == '__main__':
     if "Display" in App.listDocuments().keys():
         App.closeDocument("Display")
     doc = App.newDocument("Display")
-    display = AdafruitTFTFeatherWing("Display",Base.Vector(0,0,0))
+    display = Adafruitvl6180x("Display",Base.Vector(0,0,0))
     display.show(doc)
-    Gui.activeDocument().activeView().viewRight()
+    Gui.activeDocument().activeView().viewFront()
     Gui.SendMsgToActiveView("ViewFit")
     
